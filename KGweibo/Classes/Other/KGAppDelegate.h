499c5e803c9eed96bf48124b2f46eb74381ef7c6
mob <mob@admindeMac-mini-3.local>
2015-03-16 15:43:19 +0800

    first commit

---
+++ KGweibo/Classes/Other/KGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGAppDelegate.h
+//  KGWeibo
+//
+//  Created by mob on 15/3/16.
+//  Copyright (c) 2015年 kengsir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
