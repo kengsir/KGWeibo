be9243d017223373867b2a5920f32970bd6e2d00
mob <mob@admindeMac-mini-3.local>
2015-03-16 15:47:28 +0800

    first commit

---
+++ KGweibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KGWeibo
+//
+//  Created by mob on 15/3/16.
+//  Copyright (c) 2015年 kengsir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KGAppDelegate class]));
+    }
+}
